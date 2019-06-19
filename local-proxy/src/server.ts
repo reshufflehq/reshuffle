679394161b98f3a7e9a4ac05a38c809b49bf23e3
Roey Berman <roey.berman@gmail.com>
2019-06-19 16:35:30 +0300

    Use @babel/node to load code

--- local-proxy/src/server.ts
+++ local-proxy/src/server.ts
@@ -3,9 +3,6 @@ import { inspect } from 'util';
 import net from 'net';
 import http from 'http';
 import express, { json } from 'express';
-import { mkdtempSync } from 'fs';
-import { walk } from './walker';
-import * as rimraf from 'rimraf';
 
 const basePath = process.env.SHIFT_DEV_SERVER_BASE_REQUIRE_PATH;
 if (!basePath) {
@@ -17,32 +14,15 @@ if (!localToken) {
 }
 const app = express();
 
-let transpiled = false;
-let transpilePromise: Promise<void>;
-const readyTranspile = async () => {
-  if (transpiled) {
-    return;
-  }
-  if (transpilePromise) {
-    return await transpilePromise;
-  }
-  transpilePromise = walk(basePath, tmpDir);
-  await transpilePromise;
-  transpiled = true;
-};
-
-const tmpDir = mkdtempSync(pathJoin(basePath, '.local_proxy_'));
-
 app.post('/invoke', json(), async (req, res) => {
-  if (req.headers['x-shift-dev-server-local-token'] !== localToken) {
-    return res.sendStatus(403);
-  }
+  // if (req.headers['x-shift-dev-server-local-token'] !== localToken) {
+  //   return res.sendStatus(403);
+  // }
   try {
     // TODO: validate request
     const { path, handler, args } = req.body;
-    await readyTranspile();
     // Make sure we use tmpDir as absolute path
-    const mod = require(pathJoin(tmpDir, path));
+    const mod = require(pathJoin(basePath, path));
     const fn = mod[handler];
     // TODO: check function is exposed
     const ret = await fn(...args);
@@ -63,10 +43,4 @@ process.on('message', (m, netServer) => {
     const httpServer = http.createServer(app);
     (netServer as net.Server).on('connection', (socket) => httpServer.emit('connection', socket));
   }
-});
-
-// nodemon uses SIGUSR2
-process.once('SIGUSR2', () => {
-  rimraf.sync(tmpDir);
-  process.kill(process.pid, 'SIGUSR2');
 });
