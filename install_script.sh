92a12cdc13b146283b287491b20ecfe6c4bbba6d
Vladimir Zoubritsky <vladimir@binaris.com>
2019-06-20 17:31:16 +0300

    CR: Do not leave project in an inconsistent state
    Check that project is CRA
    Uses precise regex

--- install_script.sh
+++ install_script.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
-if [ ! -f package.json ]; then
-  echo "Can not run outside of a create-react-app project"
+if ! grep -q 'react-scripts eject' package.json; then
+  echo "Can not run outside of a create-react-app or an ejected project"
   exit 1
 fi
-npm i @binaris/shift-local-proxy
-npm i @binaris/shift-babel-macro
-npm i @binaris/shift-fetch-runtime
-npm i @binaris/shift-db
 if [ ! -f src/setupProxy.js ]; then
   cat >src/setupProxy.js <<EOF
 const { setupProxy } = require('@binaris/shift-local-proxy');
@@ -15,9 +11,14 @@ module.exports = setupProxy(__dirname);
 EOF
 else
   echo "src/setupProxy.js exists, can not install shiftjs"
+  exit 1
 fi
+npm i @binaris/shift-local-proxy
+npm i @binaris/shift-babel-macro
+npm i @binaris/shift-fetch-runtime
+npm i @binaris/shift-db
 if [ -f .gitignore ]; then
-  if ! grep '\.shift\*' .gitignore ; then echo '.shift*' >> .gitignore ; fi
+  if ! grep -q '^\.shift\*' .gitignore ; then echo '.shift*' >> .gitignore ; fi
 else
   echo ".gitignore not found, not adding .shift* to ignore"
 fi
