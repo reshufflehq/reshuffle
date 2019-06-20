6d536fa5060c1d6b47d0df50062e41c6519893d8
Vladimir Zoubritsky <vladimir@binaris.com>
2019-06-20 17:41:03 +0300

    CR: Log modified files
    Stop on failures

--- install_script.sh
+++ install_script.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
 if ! grep -q 'react-scripts eject' package.json; then
   echo "Can not run outside of a create-react-app or an ejected project"
   exit 1
@@ -9,6 +11,7 @@ const { setupProxy } = require('@binaris/shift-local-proxy');
 
 module.exports = setupProxy(__dirname);
 EOF
+  echo "Created src/setupProxy.js, please commit this file"
 else
   echo "src/setupProxy.js exists, can not install shiftjs"
   exit 1
@@ -17,8 +20,12 @@ npm i @binaris/shift-local-proxy
 npm i @binaris/shift-babel-macro
 npm i @binaris/shift-fetch-runtime
 npm i @binaris/shift-db
+echo "Modified package.json, please commit this file"
 if [ -f .gitignore ]; then
-  if ! grep -q '^\.shift\*' .gitignore ; then echo '.shift*' >> .gitignore ; fi
+  if ! grep -q '^\.shift\*' .gitignore ; then
+    echo '.shift*' >> .gitignore
+    echo "Modified .gitignore, please commit this file"
+  fi
 else
   echo ".gitignore not found, not adding .shift* to ignore"
 fi
