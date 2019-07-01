5d734776ed21d7a5213548fd9a5cfc6cc3ce2c7e
Vladimir Zoubritsky <vladimir@binaris.com>
2019-07-01 14:34:33 +0300

    CR: Add test to detect flakiness

--- local-proxy/src/index.ts
+++ local-proxy/src/index.ts
@@ -5,10 +5,20 @@ import proxy from 'http-proxy-middleware';
 import { Application } from 'express';
 import nanoid from 'nanoid';
 
+const isTestEnv = process.env.NODE_ENV === 'test';
+
+function log(message?: any, ...optionalParams: any[]) {
+  if (!isTestEnv) {
+    console.log(message, ...optionalParams);
+  }
+}
+
 // TODO(vladimir): allow overriding dev server port
 const port = 19291;
 export function startProxy(rootDir: string, localToken: string) {
-  console.log(`Dev server starting on port: ${port}`);
+  if (process.env.NODE_ENV !== 'test') {
+    log(`Dev server starting on port: ${port}`);
+  }
 
   nodemon({
     watch: [
@@ -21,18 +31,26 @@ export function startProxy(rootDir: string, localToken: string) {
       SHIFT_DEV_SERVER_BASE_REQUIRE_PATH: path.resolve(path.join(rootDir, 'backend')),
       SHIFT_DEV_SERVER_LOCAL_TOKEN: localToken,
     },
+    // Workaround for tests:
+    // server.js uses babel/dir which has a console.log
+    // AVA transaltes console.log to stderr
+    // Rush detects stderr as warnings and returns an exit code
+    stdout: process.env.NODE_ENV !== 'test',
   });
 
   nodemon.on('quit', () => {
     process.exit();
   }).on('start', (_child) => {
+    if (process.env.NODE_ENV !== 'test') {
+      log('Local dev server started');
+    }
     // this child is started
   }).on('message', (message) => {
     if (message === 'ready') {
       // this child is ready
     }
   }).on('restart', (files) => {
-    console.log('Local dev server restarted due to changes in: ', files);
+    log('Local dev server restarted due to changes in: ', files);
   });
 }
 
@@ -44,6 +62,7 @@ export function setupProxy(sourceDir: string) {
     app.use(proxy('/invoke', {
       target: `http://localhost:${port}/`,
       headers: { 'x-shift-dev-server-local-token': localToken },
+      logLevel: 'error',
     }));
   };
 }
