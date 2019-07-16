d56d91e3dfe65ad6b756e57120fe4255b848da33
Roey Berman <roey.berman@gmail.com>
2019-07-16 16:02:59 +0300

    Address CR comments, support key deletion with subscriptions

--- db/src/utils.ts
+++ db/src/utils.ts
@@ -18,3 +18,27 @@ export const withTimeout = async <T>(promise: Promise<T>, timeout: number = 5000
     clearTimeout(handle!);
   }
 };
+
+export const noop = () => undefined;
+
+export function deferred<T>() {
+  // resolve and reject have to be initialized to prevent typescript from complaining that they're uninitialized
+  let resolve: (value?: T | PromiseLike<T> | undefined) => void = noop;
+  let reject: (reason?: any) => void = noop;
+  const promise = new Promise<T>((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  });
+  return {
+    promise,
+    resolve,
+    reject,
+  };
+}
+
+const NS_PER_SEC = 1e9;
+
+export function hrnano() {
+  const [a, b] = process.hrtime();
+  return a * NS_PER_SEC + b;
+}
