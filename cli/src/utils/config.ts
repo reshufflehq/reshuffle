f10256b01964f58b8b699016ea86739626897ce6
arikmaor <arik@binaris.com>
2019-08-08 13:58:54 +0300

    Add URL generation by realm

--- cli/src/utils/config.ts
+++ cli/src/utils/config.ts
@@ -4,3 +4,11 @@ import { resolve } from 'path';
 const pjson = JSON.parse(readFileSync(resolve(__dirname, '../../package.json'), 'utf-8'));
 
 export const cliBinName = pjson.oclif.bin as string;
+
+export function getBaseUrl(subdomain: string, realm: string, protocol: 'http' | 'https' = 'https') {
+  const domain = realm === 'prod' ? 'shiftjs.com' : 'shiftjs.io';
+  const actualSubdomain = realm === 'prod' ? subdomain :
+    subdomain === 'app' ? realm :
+    `${subdomain}-${realm}`;
+  return `${protocol}://${actualSubdomain}.${domain}`;
+}
