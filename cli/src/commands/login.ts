378bb0164c6b673a507548a205e6fb436cd2cf53
arikmaor <arik@binaris.com>
2019-08-11 12:52:46 +0300

    Add custom oclif base command

--- cli/src/commands/login.ts
+++ cli/src/commands/login.ts
@@ -1,14 +1,9 @@
-import { Command } from '@oclif/command';
-import flags from '../utils/cli-flags';
+import Command from '../utils/command';
 
 export default class Login extends Command {
   public static hidden = true;
 
   public static description = 'Login to ShiftJS';
-
-  public static flags = {
-    help: flags.help({ char: 'h' }),
-  };
 
   public static args = [];
 
