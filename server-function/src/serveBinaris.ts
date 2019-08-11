6972e300a4e343cd98b768f7819aa42fb5d50693
Vladimir Zoubritsky <vladimir@shiftjs.com>
2019-08-11 12:56:18 +0300

    Convert binaris serving function to TypeScript

---
+++ server-function/src/serveBinaris.ts
@@ -0,0 +1,57 @@
+import fs from 'mz/fs';
+import { Server } from './index';
+import { resolve as pathResolve } from 'path';
+const shiftServer = new Server('./build');
+import { BinarisFunction } from './binaris';
+
+interface InvokeRequest {
+  path: string;
+  handler: string;
+  args: any[];
+}
+
+function isInvokeRequest(body: unknown): body is InvokeRequest {
+  if (body === undefined) {
+    return false;
+  }
+  const maybeInvoke = body as InvokeRequest;
+  return typeof maybeInvoke.path === 'string' &&
+    typeof maybeInvoke.handler === 'string' &&
+    Array.isArray(maybeInvoke.args);
+}
+
+export const handler: BinarisFunction = async (body, ctx) => {
+  const url = ctx.request.path;
+  const decision = await shiftServer.handle(url, ctx.request.headers);
+  switch (decision.action) {
+    case 'handleInvoke': {
+      if (!isInvokeRequest(body)) {
+        return new ctx.HTTPResponse({
+          statusCode: 400,
+        });
+      }
+      const { path, handler: fnHandler, args } = body;
+      const joinedDir = pathResolve('./backend', path);
+      const mod = require(joinedDir);
+      const fn = mod[fnHandler];
+      return fn(...args);
+    }
+    case 'sendStatus': {
+      return new ctx.HTTPResponse({
+        statusCode: decision.status,
+      });
+    }
+    case 'serveFile': {
+      const headers = { 'Content-Type': decision.contentType };
+      if (decision.cacheHint) {
+        Object.assign(headers, decision.cacheHint);
+      }
+      return new ctx.HTTPResponse({
+        statusCode: 200,
+        headers,
+        body: await fs.readFile(decision.fullPath),
+      });
+    }
+    default: throw new Error('should not get here');
+  }
+};
