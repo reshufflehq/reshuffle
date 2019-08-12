8ef961b715f720346ea33bb1b74a103fe4f326c8
arikmaor <arik@binaris.com>
2019-08-12 11:39:11 +0300

    Move cliBinName to BaseCommand

--- cli/src/commands/deploy.ts
+++ cli/src/commands/deploy.ts
@@ -1,11 +1,10 @@
 import Command from '../utils/command';
-import { cliBinName } from '../utils/config';
 
 export default class Deploy extends Command {
   public static description = 'deploy your ShiftJS project to its associated domain';
 
   public static examples = [
-    `$ ${cliBinName} deploy`,
+    `$ ${Command.cliBinName} deploy`,
   ];
 
   public static args = [];
