0d1fb19f17e86dd0bbee9c75a4a65309855effda
Vladimir Zoubritsky <vladimir@shiftjs.com>
2019-08-12 15:44:43 +0300

    Add serving tests using filesystem

--- server-function/src/test/serve.test.ts
+++ server-function/src/test/serve.test.ts
@@ -1,11 +1,36 @@
 import test, { ExecutionContext } from 'ava';
 import { Server } from '../index';
+import path from 'path';
+import fs from 'fs';
+
+const examplePath = path.join(__dirname, 'examples/index_with_static/');
 
 async function testServer(t: ExecutionContext, url: string, expected: any) {
-  const server = new Server(process.cwd());
+  const server = new Server(examplePath);
   const value = await server.handle(url, {});
   t.deepEqual(expected, value);
 }
 
 test('invoke', testServer, '/invoke', { action: 'handleInvoke' });
 test('not-found', testServer, '/invoke.jsx', { action: 'sendStatus', status: 404 });
+test('slash-is-index-html', testServer, '/', {
+  action: 'serveFile',
+  cacheHint: {},
+  contentType: 'text/html; charset=utf-8',
+  fullPath: path.join(examplePath, 'index.html'),
+});
+test('index-html-is-index-html', testServer, '/index.html', {
+  action: 'serveFile',
+  cacheHint: {},
+  contentType: 'text/html; charset=utf-8',
+  fullPath: path.join(examplePath, 'index.html'),
+});
+test('static-has-cache-hint', testServer, '/static/0.chunk.js', {
+  action: 'serveFile',
+  cacheHint: {
+    'cache-control': 'public, max-age=2629800',
+    'last-modified': fs.statSync(path.join(examplePath, 'static/0.chunk.js')).mtime.toUTCString(),
+  },
+  contentType: 'application/javascript; charset=utf-8',
+  fullPath: path.join(examplePath, 'static/0.chunk.js'),
+});
