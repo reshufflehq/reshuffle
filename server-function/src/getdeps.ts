4a3ad03e5d31774dedea2426846f3f7dac30746c
Vladimir Zoubritsky <vladimir@shiftjs.com>
2019-08-12 12:59:04 +0300

    Convert getdeps to TypeScript

--- server-function/src/getdeps.ts
+++ server-function/src/getdeps.ts
@@ -1,4 +1,4 @@
-const fs = require('fs');
+import fs from 'fs';
 const contents = JSON.parse(fs.readFileSync('package-lock.json', 'utf8'));
 const pjson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 const pjsonDeps = pjson.dependencies;
@@ -6,10 +6,14 @@ const dependencies = contents.dependencies;
 const listOfDeps = new Set();
 const toProcess = Object.keys(pjsonDeps);
 while (toProcess.length) {
-  const p = toProcess.shift();
+  const p: string = toProcess.shift()!;
+  // ignore bulk of react-scripts
   if (p === 'react-scripts') continue;
+  // temporary until shift-db implementation changes to use client instead of leveldb
   if (p === '@binaris/shift-db') continue;
+  // exclude local-proxy needed only for development
   if (p === '@binaris/shift-local-proxy') continue;
+  // exclude already visited deps
   if (listOfDeps.has(p)) continue;
   listOfDeps.add(p);
   for (const subpackage of Object.keys(dependencies[p].requires || {})) {
@@ -17,5 +21,6 @@ while (toProcess.length) {
   }
 }
 for (const dep of listOfDeps) {
+  // tslint:disable-next-line:no-console
   console.log(dep);
 }
