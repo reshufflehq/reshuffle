ca4f0d31753ed8367548170182598720e9b8d0f9
Ariel Shaqed (Scolnicov) <ariels@shiftjs.com>
2019-08-14 15:30:01 +0300

    Extract LevelDB-based server
    Most tests kept from db/, except for query -- that requires more work,
    and can wait until Thoth also supports queries (and we release that
    feature).

---
+++ leveldb-server/src/errors.ts
@@ -0,0 +1,7 @@
+export class ValueError extends Error {
+  public readonly name = 'ValueError';
+}
+
+export class IllegalArgumentError extends Error {
+  public readonly name = 'IllegalArgumentError';
+}
