ab9134d872b8f7007b8b382acb4152baea468514
Ariel Shaqed (Scolnicov) <ariels@shiftjs.com>
2019-09-04 09:48:56 +0300

    Initial CLI tests (usage)
    Substantive tests to come soon.

---
+++ cli/src/test/cli.spec.ts
@@ -0,0 +1,43 @@
+import test from 'ava';
+import { Shell, success } from 'specshell';
+import * as R from 'ramda';
+
+const process = R.evolve({ out: (x: Buffer) => x.toString(), err: (x: Buffer) => x.toString() });
+
+test('cli with no args shows VERSION, USAGE, COMMANDS', async (t) => {
+  const shell = new Shell();
+  // TODO(ariels): Find this directory!
+  const { out, err, ...status } = process(await shell.run('./bin/run'));
+
+  t.true(success(status));
+  t.assert(err === '');
+});
+
+test('cli with no args and cli --help give same output', async (t) => {
+  const shell = new Shell();
+  const noArgs = process(await shell.run('./bin/run'));
+  const helpArg = process(await shell.run('./bin/run'));
+  t.deepEqual(noArgs, helpArg);
+});
+
+// TODO(arik): Do we want to enforce a particular order?
+test('cli with no args lists all help, deploy, logs', async (t) => {
+  const expectedCommands = new Set([
+    'browse', 'claim', 'deploy', 'destroy', 'help', 'list', 'logs', 'try', 'whoami'
+  ]);
+  const shell = new Shell();
+  const { out } = process(await shell.run('./bin/run'));
+
+  const commandsMatch = out.match(/^ShiftJS CLI Tool.*?VERSION.*?USAGE.*?COMMANDS.*?\n(.*)/s);
+  t.truthy(commandsMatch, 'Match command titles');
+
+  const commandRegExp = /^\s*(\w+)/mg;
+  const commandsStr = commandsMatch![1];
+  const commands = new Set<string>();
+  let commandGroup: RegExpMatchArray | null;
+  while ((commandGroup = commandRegExp.exec(commandsStr)) != null) {
+    commands.add(commandGroup[1]);
+  }
+
+  t.deepEqual(commands, expectedCommands);
+});
