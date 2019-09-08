5c23699a1367eb7392a28af037d575668e3f96bf
Roey Berman <roey.berman@gmail.com>
2019-09-08 14:38:53 +0300

    Add JSON output to list command

--- cli/src/commands/list.ts
+++ cli/src/commands/list.ts
@@ -1,6 +1,7 @@
-import Command from '../utils/command';
-
 import columnify from 'columnify';
+
+import Command from '../utils/command';
+import flags from '../utils/cli-flags';
 
 export default class List extends Command {
   public static description = 'list applications';
@@ -12,28 +13,51 @@ export default class List extends Command {
 
   public static strict = true;
 
+  public static flags = {
+    ...Command.flags,
+    format: flags.string({
+      char: 'f',
+      description: 'Format output',
+      default: 'table',
+      options: ['table', 'json'],
+    }),
+  };
+
   public async run() {
-    this.parse(List);
+    const { format } = this.parse(List).flags;
     await this.authenticate();
     const apps = await this.lycanClient.listApps();
-    if (apps.length === 0) {
-      this.log('You do not have any apps yet.');
-      return;
-    }
-    this.log(columnify(apps.map(({ name, updatedAt, environments }) => ({
-      name, updatedAt: updatedAt.toISOString(), URL: `https://${environments[0].domains[0].name}`,
-    })), {
-      columns: ['name', 'updatedAt', 'URL'],
-      config: {
-        name: {
-          headingTransform: () => 'APPLICATION',
-          minWidth: 25,
-        },
-        updatedAt: {
-          headingTransform: () => 'LAST UPDATED',
-          minWidth: 25,
-        },
-      },
+    const mappedApps = apps.map(({ name, updatedAt, environments }) => ({
+      name,
+      updatedAt: updatedAt.toISOString(),
+      URL: `https://${environments[0].domains[0].name}`,
     }));
+
+    switch (format) {
+      case 'table':
+        if (apps.length === 0) {
+          this.log('You do not have any apps yet.');
+          return;
+        }
+        this.log(columnify(mappedApps, {
+          columns: ['name', 'updatedAt', 'URL'],
+          config: {
+            name: {
+              headingTransform: () => 'APPLICATION',
+              minWidth: 25,
+            },
+            updatedAt: {
+              headingTransform: () => 'LAST UPDATED',
+              minWidth: 25,
+            },
+          },
+        }));
+        return;
+      case 'json':
+        this.log(JSON.stringify(mappedApps));
+        return;
+      default:
+        throw new Error(`Invalid output format requested: ${format}`);
+    }
   }
 }
