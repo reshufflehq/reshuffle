21ffc76cb44e30a569ace6350868b9555fa82a96
Ariel Shaqed (Scolnicov) <ariels@shiftjs.com>
2019-09-09 14:28:45 +0300

    Add snapshot-based whoami CLI unitest

---
+++ cli/src/test/whoami.spec.ts
@@ -0,0 +1,24 @@
+import anyTest, { TestInterface } from 'ava';
+import * as td from 'testdouble';
+import { Context, addFake } from './fake_lycan';
+import * as R from 'ramda';
+
+const test = anyTest as TestInterface<Context>;
+
+addFake(test);
+
+const process = R.evolve({ out: (x: Buffer) => x.toString(), err: (x: Buffer) => x.toString() });
+
+const anything = td.matchers.anything();
+
+test('browse lists templates on dumb terminal', async (t) => {
+  td.when(t.context.lycanFake.whoami(anything)).thenResolve(
+    {
+      id: 'fluffy',
+      fullName: 'You N. Icorn',
+      email: 'horn@hoof.invalid',
+    });
+
+  const result = process(await t.context.shell.run('./bin/run whoami'));
+  t.snapshot(result);
+});
