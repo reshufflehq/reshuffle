c988933a4c059ac0d5c2df687a13caed50ba3467
Ariel Shaqed (Scolnicov) <ariels@shiftjs.com>
2019-09-10 17:05:55 +0300

    Diagnose bad server responses

--- cli/src/test/whoami.spec.ts
+++ cli/src/test/whoami.spec.ts
@@ -1,6 +1,7 @@
 import anyTest, { TestInterface } from 'ava';
 import * as td from 'testdouble';
 import { Context, addFake } from './fake_lycan';
+import { success } from 'specshell';
 
 const test = anyTest as TestInterface<Context>;
 
@@ -19,4 +20,14 @@ test('browse lists templates on dumb terminal', async (t) => {
 
   const result = await t.context.shell.run(`${t.context.run} whoami`, 'utf-8');
   t.snapshot(result);
+});
+
+test('whoami (and other commands) receiving incomprehensible server reply complains nicely', async (t) => {
+  td.when(t.context.lycanFake.whoami(anything)).thenResolve(
+    ['cannot', 'understand', 'this', 'response']
+  );
+
+  const result = await t.context.shell.run(`${t.context.run} whoami`, 'utf-8');
+  t.assert(!success(result));
+  t.assert(result.err.match(/Failed to communicate with server: .*\..*Please run "npm update -g shift-cli"\./s));
 });
