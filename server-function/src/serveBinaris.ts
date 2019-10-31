516a5708c7598c8a4d471bb2542c698c588c2ff0
Roey Berman <roey.berman@gmail.com>
2019-10-31 11:11:54 +0200

    Use sendFile instead of sendfile

--- server-function/src/serveBinaris.ts
+++ server-function/src/serveBinaris.ts
@@ -58,7 +58,7 @@ app.post('/invoke', express.json(), async (req, res) => {
 });
 
 app.use(express.static(buildDir));
-app.use((_req, res) => res.sendfile(pathResolve(buildDir, 'index.html')));
+app.use((_req, res) => res.sendFile(pathResolve(buildDir, 'index.html')));
 
 export type HTTPHandler = (req: http.IncomingMessage, res: http.ServerResponse) => void;
 
