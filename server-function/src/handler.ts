6bff1222e6dca5168d20835753f3341fde7de568
Roey Berman <roey.berman@gmail.com>
2019-10-31 11:03:37 +0200

    Add logging to help debug user HTTP handler

--- server-function/src/handler.ts
+++ server-function/src/handler.ts
@@ -16,7 +16,7 @@ export class UnauthorizedError extends HandlerError {
 
 export type Handler = (...args: any) => any;
 
-export function getHandler(backendDir: string, fnPath: string, handler: string): Handler {
+export function getHandler(backendDir: string, fnPath: string, handler: string, checkExposed: boolean = true): Handler {
   const joinedDir = path.resolve(backendDir, fnPath);
   if (path.relative(backendDir, joinedDir).startsWith('..')) {
     throw new UnauthorizedError(`Cannot reference path outside of root dir: ${fnPath}`);
@@ -24,8 +24,31 @@ export function getHandler(backendDir: string, fnPath: string, handler: string):
   const mod = require(joinedDir);
   const fn = mod[handler];
   // Cast to any so typescript doesn't complain against accessing __reshuffle__ on a function
-  if (!(typeof fn === 'function' && (fn as any).__reshuffle__ && (fn as any).__reshuffle__.exposed)) {
+  if (typeof fn !== 'function') {
+    throw new HandlerError(`${fnPath}.${handler} is not a function`, 400);
+  }
+  const { __reshuffle__ } = fn;
+  if (checkExposed && !(__reshuffle__ && __reshuffle__.exposed)) {
     throw new  UnauthorizedError(`Cannot invoke ${fnPath}.${handler} - not an exposed function`);
   }
   return fn;
+}
+
+function resolveHandlerPath(backendDir: string, handler: string) {
+  try {
+    return require.resolve(path.resolve(backendDir, handler));
+  } catch (err) {
+    return undefined;
+  }
+}
+
+export function getHTTPHandler(backendDir: string) {
+  const handlerPath = resolveHandlerPath(backendDir, '_handler');
+  if (handlerPath === undefined) {
+    return undefined;
+  }
+  return {
+    fn: getHandler(backendDir, '_handler', 'default', false),
+    path: handlerPath,
+  };
 }
--- server-function/src/index.ts
+++ server-function/src/index.ts
@@ -1,5 +1,12 @@
 import { defaultHandler, HTTPHandler } from './serveBinaris';
-export { Handler, HandlerError, UnauthorizedError, getHandler } from './handler';
+export {
+  Handler,
+  HandlerError,
+  UnauthorizedError,
+  getHandler,
+  getHTTPHandler,
+} from './handler';
+
 export { HTTPHandler, defaultHandler };
 
 export function setHTTPHandler(override: HTTPHandler) {
