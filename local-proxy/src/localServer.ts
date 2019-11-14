ae176a89f7fe31b15a7c259d27716bef7f33d5cb
Vladimir Zoubritsky <vladimir@reshuffle.com>
2019-11-14 11:17:02 +0200

    Add reshuffle-local-server helper script
    For running without create-react-app
    Used in a package.json as { "start": "reshuffle-local-server" }

---
+++ local-proxy/src/localServer.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+// tslint:disable:no-console
+import express from 'express';
+import { setupProxy } from './index';
+import program from 'commander';
+const NAME = 'reshuffle-local-server';
+program.option('-p, --port', 'listening port (3000 by default)');
+program.description(`Run the ${NAME} outside of create-react-app server`);
+
+function main() {
+  program.parse(process.argv);
+  // create the express app to route local requests
+  const app = express();
+
+  // reshuffle currently assumes running from src and uses
+  // a relative path resolution to reach backend
+  setupProxy(process.cwd() + '/src')(app);
+
+  const port = program.port === undefined ? 3000 : +program.port;
+  const server = app.listen(port);
+  server.on('error', (err) => {
+    if ((err as NodeJS.ErrnoException).code !== 'EADDRINUSE') {
+      throw err;
+    }
+    console.error(`Error running ${NAME} on port ${port} - EADDRINUSE the port is used by a different program`);
+    process.exit(1);
+  });
+  server.on('listening', () => {
+    console.log(`${NAME} is listening on port ${port}`);
+  });
+}
+
+main();
