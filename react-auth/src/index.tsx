516ca34803ade248c3f1457337989a9dd10df725
Ariel Shaqed (Scolnicov) <ariels@reshuffle.com>
2019-11-21 14:42:29 +0200

    More robust "Content-Type: application/json" test (this one works)

--- react-auth/src/index.tsx
+++ react-auth/src/index.tsx
@@ -47,8 +47,8 @@ export const AuthProvider: FC = ({ children }) => {
     if (!req.ok) {
       throw new Error(`Failed to get /whoami ${req.statusText}`);
     }
-    if (req.headers.get('content-type') !== 'application/json') {
-      throw new Error('Failed to GET /whoami JSON data: backend may be missing _handler.js.');
+    if (!req.headers.get('content-type')!.startsWith('application/json')) {
+      throw new Error('Failed to GET /whoami JSON data: backend may be missing file _handler.js.');
     }
     return req.json();
   }, []);
