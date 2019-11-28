21cbad85411d85c47456fd210b7044ed275f9003
Michael Adda <michael@reshuffle.com>
2019-11-28 16:45:33 +0200

    Add hidden info command

---
+++ cli/src/commands/info.ts
@@ -0,0 +1,56 @@
+import { inspect } from 'util';
+import { Application } from '@binaris/spice-node-client/interfaces';
+
+import Command from '../utils/command';
+import {
+  findProjectByDirectory,
+  getProjectRootDir,
+} from '../utils/helpers';
+
+export default class Info extends Command {
+  public static description = 'get application info';
+  public static hidden = true;
+  public static examples = [
+    `$ ${Command.cliBinName} info`,
+    `$ ${Command.cliBinName} info neat-bison-17`,
+  ];
+
+  public static strict = true;
+
+  public static args = [
+    {
+      name: 'name',
+      required: false,
+      description: 'Application name (defaults to working directory\'s deployed application name)',
+    },
+  ];
+
+  protected async getAppDetails(appName?: string): Promise<Application> {
+    if (appName) {
+      try {
+        return await this.lycanClient.getAppByName(appName);
+      } catch (e) {
+        return this.error(`Cannot find application ${appName}`);
+      }
+    } else {
+      const projectDir = await getProjectRootDir();
+      const projects = this.conf.get('projects');
+      const project = findProjectByDirectory(projects, projectDir);
+      if (project === undefined) {
+        return this.error(`No project deployments found, please run ${Command.cliBinName} deploy`);
+      }
+      const { applicationId } = project;
+      try {
+        return await this.lycanClient.getApp(applicationId);
+      } catch (e) {
+        return this.error('Cannot find local application');
+      }
+    }
+  }
+  public async run() {
+    const { name: appName } = this.parse(Info).args;
+    await this.authenticate();
+    const application = await this.getAppDetails(appName);
+    this.log(inspect(application, { depth: Infinity, compact: false }));
+  }
+}
