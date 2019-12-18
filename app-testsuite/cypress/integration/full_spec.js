7a4425dd88331a52128e051b29d0a18e0ab8b29a
Roey Berman <roey.berman@gmail.com>
2019-12-18 12:39:00 +0200

    Add react-storage and storage E2E test

--- app-testsuite/cypress/integration/full_spec.js
+++ app-testsuite/cypress/integration/full_spec.js
@@ -2,7 +2,7 @@ describe('counter', () => {
   it('gets initial data and increments on click', () => {
     cy.visit('/');
     cy.get('.counter').contains('0');
-    cy.get('input').click();
+    cy.get('.counterContainer input').click();
     cy.get('.counter').contains('1');
   });
 });
@@ -31,5 +31,23 @@ describe('user HTTP handler', () => {
     cy.visit('/express');
     cy.get('.express').contains('hello from express');
     cy.get('.express-variables').contains('url:/variables originalUrl:/express/variables baseUrl:/express');
+  });
+});
+
+describe('upload', () => {
+  it('successfully uploads a file', () => {
+    cy.visit('/');
+    cy.fixture('reshuffle.png', 'base64').then((fileContent) => {
+      cy.get('.upload input').upload(
+        { fileContent, fileName: 'reshuffle.png', mimeType: 'image/png' },
+        { subjectType: 'input' },
+      );
+      cy.get('.upload img')
+        .should('be.visible')
+        .and(($img) => {
+          // "naturalWidth" and "naturalHeight" are set when the image loads
+          expect($img[0].naturalWidth).to.be.greaterThan(0);
+        });
+    });
   });
 });
