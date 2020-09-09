197016bc544223ec5382115866653b898c7c916b
christophe gachiniard <christophe@reshuffle.com>
2020-09-10 10:58:33 +1200

    Add send file example listening on /test

---
+++ examples/http/HTTPSendFileExample.js
@@ -0,0 +1,15 @@
+const {HttpConnector, Reshuffle} = require('../..');
+
+const app = new Reshuffle();
+const connector = new HttpConnector();
+const path = require('path')
+
+app.register(connector);
+
+app.when(connector.on({'method': 'GET', 'path': '/test'}), async (event) =>
+  new Promise(
+    (resolve, reject) => event.context.res.sendFile(path.resolve("./test.html"),
+      error => error ? reject() : resolve()))
+);
+
+app.start();
