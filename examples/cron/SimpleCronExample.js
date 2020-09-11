da22ef8c64e456dc20642c7f422ea61bd36b112d
gabeeden <14077091+gabeeden@users.noreply.github.com>
2020-09-11 16:11:59 +1200

    format example

--- examples/cron/SimpleCronExample.js
+++ examples/cron/SimpleCronExample.js
@@ -1,15 +1,15 @@
-const {Reshuffle, CronConnector} = require('../..');
-const app = new Reshuffle();
+const { Reshuffle, CronConnector } = require('../..')
+const app = new Reshuffle()
 
-const connector = new CronConnector(app);
+const connector = new CronConnector(app)
 
 const logger = app.getLogger()
 
 // eslint-disable-next-line no-unused-vars
-connector.on({ expression: "*/1 * * * * *" }, (event) => {
-  logger.info('an info');
-  logger.warn('a warning');
-  logger.error('an error');
+connector.on({ expression: '*/1 * * * * *' }, (event) => {
+  logger.info('an info')
+  logger.warn('a warning')
+  logger.error('an error')
 })
 
 app.start()
