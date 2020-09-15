4383b39f4f224247a40960f68497f58eee427190
christophe gachiniard <christophe@reshuffle.com>
2020-09-16 10:51:04 +1200

    Twilio - example of how to extract request params

--- examples/message/TwilioSendMessageExample.js
+++ examples/message/TwilioSendMessageExample.js
@@ -14,8 +14,33 @@ const twilioConnector = new TwilioConnector(
 // Instructions on how to test Twilio webhook locally: https://www.twilio.com/docs/usage/tutorials/how-to-set-up-your-node-js-and-express-development-environment#install-ngrok-for-local-development
 // To setup webhooks in Twilio, go to https://www.twilio.com/console/phone-numbers/incoming, select phone number, scroll all the way down to messaging, add method and path, save
 twilioConnector.on({method:'POST', path:'/sms'}, (event) => {
+  console.log(event.context.req.body)
+  // Example of console output:
+  // {
+  //   ToCountry = "US"
+  //   ToState = "ID"
+  //   SmsMessageSid = "SM9aca55a393c120f964cc49d91bfec52e"
+  //   NumMedia = "0"
+  //   ToCity = "DESMET"
+  //   FromZip = "85004"
+  //   SmsSid = "SM9aca55a393c120f964cc49d91bfec52e"
+  //   FromState = "AZ"
+  //   SmsStatus = "received"
+  //   FromCity = "PHOENIX"
+  //   Body = "msg line 1 line 2 line 3"
+  //   FromCountry = "US"
+  //   To = "+12082685987"
+  //   ToZip = "99128"
+  //   NumSegments = "1"
+  //   MessageSid = "SM9aca55a393c120f964cc49d91bfec52e"
+  //   AccountSid = "AC43820350f399443f2ab9a80ce59dc797"
+  //   From = "+19282275501"
+  //   ApiVersion = "2010-04-01"
+  // }
+
   const messageReceived = event.context.req.body.Body
-  console.log('message received:', messageReceived)
+  const fromPhoneNumber = event.context.req.body.From
+  console.log(`New SMS received from ${fromPhoneNumber}: ${messageReceived}`)
 
   if(messageReceived.includes('test')) {
     event.context.res.end("test successful")
