462a64bca8db98483d11c0b21d0bf1104fa7e495
gabeeden <14077091+gabeeden@users.noreply.github.com>
2020-09-22 11:27:27 +1200

    add nlp examples

---
+++ examples/nlp/NLPLanguageExample.js
@@ -0,0 +1,16 @@
+const { Reshuffle } = require('reshuffle')
+const { NlpConnector } = require('reshuffle-nlp-connector')
+
+const app = new Reshuffle()
+
+const nlpConnector = new NlpConnector()
+
+app.start()
+
+function main() {
+  const text = 'The text I want analyzed'
+  const result = nlpConnector.language(text)
+  console.log('Name: ', result.name, ' Code: ', result.code)
+}
+
+main()
