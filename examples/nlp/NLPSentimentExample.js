495d759af9a2b7b94dcd3cfd3b33b96cec1be339
gabeeden <14077091+gabeeden@users.noreply.github.com>
2020-09-22 11:27:27 +1200

    add nlp examples

---
+++ examples/nlp/NLPSentimentExample.js
@@ -0,0 +1,15 @@
+const { Reshuffle } = require('reshuffle')
+const { NlpConnector } = require('reshuffle-nlp-connector')
+
+const app = new Reshuffle()
+
+const nlpConnector = new NlpConnector()
+
+app.start()
+
+async function main() {
+  const result = await nlpConnector.sentiment("My text to analyze")
+  console.log("Score: ", result.score, " Vote: ", result.vote, " Emoji: ", result.emoji)
+}
+
+main()
