a30cf2b670d3357926734d86c1e227cf77bb8b73
gabeeden <14077091+gabeeden@users.noreply.github.com>
2020-10-27 16:29:15 +1300

    update example

--- examples/monday/Monday.js
+++ examples/monday/Monday.js
@@ -2,39 +2,34 @@ const { Reshuffle } = require('reshuffle')
 const { MondayConnector } = require('reshuffle-monday-connector')
 
 const BOARD_ID = 8948594
-const MY_TOKEN = ''
+const MY_TOKEN = 'MY_TOKEN'
+const BASE_URL = 'https://example.com'
+const PATH = '/monday-endpoint'
+const EVENT_TYPE = 'change_column_value'
 
 const app = new Reshuffle()
 const connector = new MondayConnector(app, {
   token: MY_TOKEN,
 })
 
-const mc = connector.on(
+connector.on(
   {
-    path: '/test-monday-endpoint',
-    baseUrl: 'https://example.com',
-    eventType: 'change_column_value',
+    path: PATH,
+    baseUrl: BASE_URL,
+    eventType: EVENT_TYPE,
     boardId: BOARD_ID,
   },
   ({ req }) => {
-    console.log(JSON.stringify(req.body))
+    console.log('Column value changed: ', JSON.stringify(req.body))
   },
 )
-
-setInterval(() => connector.removeEvent(mc), 15000)
 
 async function main() {
   try {
     const board = await connector.getBoard(BOARD_ID)
     const itemIds = board.boards[0].items.map((x) => Number(x.id))
     const items = await connector.getItem(itemIds)
-    const groupId = board.boards[0].groups[0].id
-    console.log(groupId)
-    const group = await connector.getGroup(BOARD_ID, groupId)
-    const newItem = await connector.createItem(BOARD_ID, 'my test new item')
-    console.log(JSON.stringify(items))
-    console.log(JSON.stringify(group))
-    console.log(JSON.stringify(newItem))
+    console.log('My items: ', items)
   } catch (error) {
     console.log(error)
   }
