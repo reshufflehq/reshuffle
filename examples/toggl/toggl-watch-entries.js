fca222d768efbe3e31e59e0a69573965e86d60c7
zacmos <zac@reshuffle.com>
2020-12-01 11:17:42 +1300

    toggl-example

--- examples/toggl/toggl-watch-entries.js
+++ examples/toggl/toggl-watch-entries.js
@@ -4,15 +4,15 @@ const { TogglConnector } = require('reshuffle-toggl-connector')
 const app = new Reshuffle()
 
 // https://support.toggl.com/en/articles/3116844-where-is-my-api-token-located
-const togglConnector = new TogglConnector(app,  { token: 'TOGGL_API_TOKEN' })
+const togglConnector = new TogglConnector(app, { token: 'TOGGL_API_TOKEN' })
 
 togglConnector.on({ type: 'TimeEntryAdded' }, async (event, app) => {
-    console.log('TimeEntryAdded event ' + JSON.stringify(event))
-    console.log('TimeEntry Added:')
-    console.log('Id:', event.id)
-    console.log('Start:', event.start)
-    console.log('Stop:', event.stop)
-    console.log('Description:', event.description)
+  console.log('TimeEntryAdded event ' + JSON.stringify(event))
+  console.log('TimeEntry Added:')
+  console.log('Id:', event.id)
+  console.log('Start:', event.start)
+  console.log('Stop:', event.stop)
+  console.log('Description:', event.description)
 })
 
 togglConnector.on({ type: 'TimeEntryModified' }, async (event, app) => {
@@ -25,12 +25,12 @@ togglConnector.on({ type: 'TimeEntryModified' }, async (event, app) => {
 })
 
 togglConnector.on({ type: 'TimeEntryRemoved' }, async (event, app) => {
-    console.log('TimeEntryRemoved event ' + JSON.stringify(event))
-    console.log('TimeEntry Removed:')
-    console.log('Id:', event.id)
-    console.log('Start:', event.start)
-    console.log('Stop:', event.stop)
-    console.log('Description:', event.description)
+  console.log('TimeEntryRemoved event ' + JSON.stringify(event))
+  console.log('TimeEntry Removed:')
+  console.log('Id:', event.id)
+  console.log('Start:', event.start)
+  console.log('Stop:', event.stop)
+  console.log('Description:', event.description)
 })
-  
+
 app.start(8000)
