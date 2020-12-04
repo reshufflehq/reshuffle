e119ff31f81fdac731e852126c1c54bbda621016
gabeeden <14077091+gabeeden@users.noreply.github.com>
2020-12-04 14:17:01 +1300

    add comments

--- examples/github/GitHub.js
+++ examples/github/GitHub.js
@@ -2,11 +2,15 @@ const { Reshuffle } = require('reshuffle')
 const { GitHubConnector } = require('reshuffle-github-connector')
 
 const app = new Reshuffle()
+
 const connector = new GitHubConnector(app, {
+  // See https://github.com/reshufflehq/reshuffle-github-connector/blob/master/README.md for instructions to obtain a token
   token: process.env.TOKEN,
+  // Base URL of your reshuffle app
   runtimeBaseUrl: process.env.RUNTIME_BASE_URL,
 })
 
+// Listen to push events for the reshuffle repository
 connector.on(
   {
     owner: 'reshufflehq',
@@ -18,6 +22,7 @@ connector.on(
 )
 
 async function main() {
+  // Get the issues for the reshufflehq/reshuffle repo
   const repoIssues = await connector.sdk().issues.get({
     owner: 'reshufflehq',
     repo: 'reshuffle',
@@ -26,5 +31,7 @@ async function main() {
   console.log(JSON.stringify(repoIssues.data))
 }
 
+// Start the reshuffle app
 app.start()
+
 main()
