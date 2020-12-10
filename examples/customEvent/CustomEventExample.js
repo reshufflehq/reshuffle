541f72d77c763136d6e13fa8687539e1e2aad4fc
zacmos <zac@reshuffle.com>
2020-12-11 10:24:32 +1300

    RS-190

--- examples/customEvent/CustomEventExample.js
+++ examples/customEvent/CustomEventExample.js
@@ -3,19 +3,13 @@ const { CustomEventConnector, Reshuffle } = require('../..')
 const app = new Reshuffle()
 const connector = new CustomEventConnector(app)
 
-connector.on(
-  { channel:'One', payLoad: {name: 'Jack', age: 25} },
-  (event) => {
-    console.log('Custom Event One: ', event.options)
-  },
-)
+connector.on({ channel: 'One', payLoad: { name: 'Jack', age: 25 } }, (event) => {
+  console.log('Custom Event One: ', event.options)
+})
 
-connector.on(
-  { channel:'Two', payLoad: 'Just a String' },
-  (event) => {
-    console.log('Custom Event Two: ', event.options)
-  },
-)
+connector.on({ channel: 'Two', payLoad: 'Just a String' }, (event) => {
+  console.log('Custom Event Two: ', event.options)
+})
 async function main() {
   connector.fire('One')
   connector.fire('Two')
