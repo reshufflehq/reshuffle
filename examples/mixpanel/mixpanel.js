1c0dfa71b49b6c03a337ed93e278ad0a0379b375
christophe gachiniard <christophe@reshuffle.com>
2020-12-23 09:59:10 +1300

    RS-202 - mixpanel example

--- examples/mixpanel/mixpanel.js
+++ examples/mixpanel/mixpanel.js
@@ -6,27 +6,26 @@ const { MixpanelConnector } = require('reshuffle-mixpanel-connector')
 
 const app = new Reshuffle()
 const mixpanelConnector = new MixpanelConnector(app, {
-  token: '56991289116de4d6ab5703ed2ee45a21',
-  secret: '4dcae6b37d58049e1c532c02db400f83',
+  token: '<mixpanel-token>',
+  secret: '<mixpanel-secret>',
 })
-
-async function main() {
-  mixpanelConnector.track('latlong', {
-    key1: 'value1',
-    key2: 'value2',
-    lat: 40.800875,
-    long: -73.945678,
-  })
-
-  const fiveDaysAgo = new Date() - 1000 * 60 * 60 * 24 * 5
-  mixpanelConnector.import('imported event', fiveDaysAgo, {
-    key1: 'import value1',
-    key2: 'import value2',
-    lat: 40.800431,
-    long: -73.957127,
-  })
-}
 
 app.start()
 
-main()
+// Track a new event in Mixpanel
+mixpanelConnector.track('test', {
+  key1: 'value1',
+  key2: 'value2',
+  lat: 40.800875,
+  long: -73.945678,
+})
+
+const fiveDaysAgo = new Date() - 1000 * 60 * 60 * 24 * 5
+
+// Import an event in Mixpanel
+mixpanelConnector.import('imported event', fiveDaysAgo, {
+  key1: 'import value1',
+  key2: 'import value2',
+  lat: 40.800431,
+  long: -73.957127,
+})
