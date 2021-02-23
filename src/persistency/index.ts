64229d73d3d4e1266ed7f4b6d0198a3baf45b258
zacmos <zac@reshuffle.com>
2021-02-23 14:02:32 +1300

    mysql-store

--- src/persistency/index.ts
+++ src/persistency/index.ts
@@ -2,7 +2,8 @@ import PersistentStore from './PersistentStore'
 import FileStoreAdapter from './FileStoreAdapter'
 import MemoryStoreAdapter from './MemoryStoreAdatper'
 import SQLStoreAdapter from './SQLStoreAdapter'
+import MySQLStoreAdapter from './MySQLStoreAdapter'
 
 export * from './types'
 
-export { PersistentStore, FileStoreAdapter, MemoryStoreAdapter, SQLStoreAdapter }
+export { PersistentStore, FileStoreAdapter, MemoryStoreAdapter, SQLStoreAdapter, MySQLStoreAdapter }
